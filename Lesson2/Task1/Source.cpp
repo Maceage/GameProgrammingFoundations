587858da00b2e57d1d8406415d9227cdd5ac797c
Graham Mace <graham@maceage.com>
2025-03-10 15:44:07 +1100

    Tidy up code

--- Lesson2/Task1/Source.cpp
+++ Lesson2/Task1/Source.cpp
@@ -7,27 +7,28 @@ int main()
 	// Variables
 	int firstNumber = 0;
 	int secondNumber = 0;
-	int operationChoice = 0;
-	int result = 0;
 
 	// Ask the user to enter two numbers
-	std::cout << "Enter a first whole number (1-10): ";
-	std::cin >> firstNumber;
+	cout << "Enter a first whole number (1-10): ";
+	cin >> firstNumber;
 
-	std::cout << "\nEnter a second whole number (1-10): ";
-	std::cin >> secondNumber;
+	cout << "\nEnter a second whole number (1-10): ";
+	cin >> secondNumber;
 
 	if (firstNumber >= 1 && firstNumber <= 10
 		&& secondNumber >= 1 && secondNumber <= 10)
 	{
-		// Ask the user which operation they would like to perform
-		std::cout << "Select an operation to perform on the two numbers:\n";
-		std::cout << "1. Addition\n";
-		std::cout << "2. Subtraction\n";
-		std::cout << "3. Multiplication\n";
-		std::cout << "4. Division\n\n";
+		int result = 0;
+		int operationChoice = 0;
 
-		std::cout << "User entered: ";
+		// Ask the user which operation they would like to perform
+		cout << "Select an operation to perform on the two numbers:\n";
+		cout << "1. Addition\n";
+		cout << "2. Subtraction\n";
+		cout << "3. Multiplication\n";
+		cout << "4. Division\n\n";
+
+		cout << "User entered: ";
 		cin >> operationChoice;
 
 		// Perform the selected operation
@@ -51,6 +52,11 @@ int main()
 			case 4: // Division
 			{
 				result = firstNumber / secondNumber;
+				break;
+			}
+			default: // Invalid operation
+			{
+				cout << "Invalid operation selected\n\n";
 				break;
 			}
 		}
